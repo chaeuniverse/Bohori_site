90923ee74fa0a4c47c337f89a5780b3decaf8b38
chaeyun <127290631+chaeuniverse@users.noreply.github.com>
2023-03-14 16:48:51 +0900

    Add files via upload

---
+++ js/bohori.js
@@ -0,0 +1,16 @@
+$(function () {
+
+
+
+    $('.mainSlide').slick({
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        prevArrow: false,
+        nextArrow: false,
+        TransitionEvent: 0.5,
+    });
+
+});
